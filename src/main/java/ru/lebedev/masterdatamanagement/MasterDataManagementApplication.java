f59b17884cd8894a244fcca5ef2f0481245b07f7
maxim-lebedev <cyue.alle@gmail.com>
2021-03-27 19:25:22 +0300

    init commit

---
+++ src/main/java/ru/lebedev/masterdatamanagement/MasterDataManagementApplication.java
@@ -0,0 +1,13 @@
+package ru.lebedev.masterdatamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDataManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lebedev/masterdatamanagement/MasterDataManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lebedev.masterdatamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterDataManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
