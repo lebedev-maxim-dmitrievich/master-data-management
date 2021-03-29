7ed22ea187c8f23def4f56d42d66beb3400c0c58
maxim-lebedev <cyue.alle@gmail.com>
2021-03-29 12:17:57 +0300

    accepting a message from another service using kafka (service-work)

--- src/main/java/ru/lebedev/masterdatamanagement/MasterDataManagementApplication.java
+++ src/main/java/ru/lebedev/masterdatamanagement/MasterDataManagementApplication.java
@@ -2,12 +2,13 @@ package ru.lebedev.masterdatamanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class MasterDataManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MasterDataManagementApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MasterDataManagementApplication.class, args);
+    }
 }
